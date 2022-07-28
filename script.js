e4132f36197e487cd49ccf7ab01a19760aa27719
rheacezil <lamurinrhea@gmail.com>
2022-07-28 20:27:08 +0800

    added all functions of buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let num1;
 let num2;
 let operation;
 let result;
+let display = document.getElementById("display");
 
 function clickHandler(input) {
     console.log(input);
@@ -11,26 +12,17 @@ function clickHandler(input) {
         } else {
             num1 = num1 + input;
         }
+        updateDisplay(num1);
     } else {
         if (num2 === undefined) {
             num2 = input;
         } else {
             num2 = num2 + input;
         }
+        updateDisplay(num2);
     }
 }
 
-function specialSymbol(input) {
-    console.log(input);
-    switch (input) {
-        case "C":
-            num1 = undefined;
-            num2 = undefined;
-            operation = undefined;
-            result = undefined;
-            break;
-    }
-}
 
 function clickOperation(input) {
     console.log(input);
@@ -47,26 +39,60 @@ function clickOperation(input) {
         case "÷":
             operation = "÷"
             break;
-        case "=":
-            switch (operation) {
-                case "+":
-                    result = Number(num1) + Number(num2);
-                    break;
-                case "-":
-                    result = Number(num1) - Number(num2);
-                    break;
-                case "X":
-                    result = Number(num1) * Number(num2);
-                    break;
-                case "÷":
-                    result = Number(num1) / Number(num2);
-                
-            }
-            console.log(result);
-            num1 = undefined;
-            num2 = undefined;
-            operation = undefined;
+    }   
+}
+
+function calculate() {
+    switch (operation) {
+        case "+":
+            result = Number(num1) + Number(num2);
             break;
+        case "-":
+            result = Number(num1) - Number(num2);
+            break;
+        case "X":
+            result = Number(num1) * Number(num2);
+            break;
+        case "÷":
+            result = Number(num1) / Number(num2);
+        
     }
-    
+    updateDisplay(result);
+    console.log(result);
+    num1 = undefined;
+    num2 = undefined;
+    operation = undefined;
+}
+
+
+function clearDisplay() {
+    updateDisplay("0");
+    num1 = undefined;
+    num2 = undefined;
+    operation = undefined;
+    result = undefined;
+}
+
+function changeSign() {
+    if (operation === undefined) {
+        num1 = Number(num1) * -1;
+        updateDisplay(num1);
+    } else {
+        num2 = Number(num2) * -1;
+        updateDisplay(num2);
+    }
+}
+
+function switchToPercent() {
+    if (operation === undefined) {
+        num1 = num1 * 0.01;
+        updateDisplay(num1);
+    } else {
+        num2 = num2 * 0.01;
+        updateDisplay(num2);
+    }
+}
+
+function updateDisplay(number) {
+    display.innerHTML = number;
 }
